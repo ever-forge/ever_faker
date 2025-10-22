329f495b6227fb3ae65a38508536ec311831b918
Zeke Dou <zeke@zekedou.live>
2025-10-22 12:27:58 +0800

    chore: remove start_permanent from mix.exs

--- mix.exs
+++ mix.exs
@@ -11,7 +11,6 @@ defmodule Faker.Mixfile do
       app: :ever_faker,
       version: @version,
       elixir: "~> 1.18",
-      start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: @description,
       source_url: @source_url,
